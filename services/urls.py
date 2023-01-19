bd515f6623189018a3ea4dce2b970bd105faa2c9
subedianusha98 <anushasubedi20@gmail.com>
2023-01-19 21:31:48 +0545

    templates added

--- services/urls.py
+++ services/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home-page"),
     path('service-search/', views.services_search, name="job-search"),
-    path('service-detail/', views.services_detail, name="job-detail")
+    path('service-detail/', views.services_detail, name="job-detail"),
+    path('service-search-map/',views.service_search_map, name="service-search-map"),
+    path('candidates/candidate-detail/', views.candidate_detail, name="candidate-detail"),
+    path('error-page/', views.error_page, name="404-page"),
+   
 ]
 
--- services/views.py
+++ services/views.py
@@ -23,3 +23,18 @@ def services_search(request):
 
 def services_detail(request):
     return render(request, 'services/service-detail.html', context={})
+
+
+def service_search_map(request):
+    template_name='services/service-search-map.html'
+    context={}
+    return render(request, template_name, context)
+
+
+def candidate_detail(request):
+    template_name='services/candidate-detail.html'
+    context={}
+    return render(request, template_name, context)
+
+def error_page(request):
+  return render(request, 'services/error-page.html', context={})
