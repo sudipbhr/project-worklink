224c71f596a25d44407955962c32bbc7243d0d12
Sudip <sudipbhandari67@gmail.com>
2023-01-22 09:39:40 +0545

    working on authentication

--- authentication/models.py
+++ authentication/models.py
@@ -4,11 +4,13 @@ from account.models import User
 # Create your models here.
 class UserVerification(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email_verified = models.BooleanField(default=False)
-    phone_verified = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=True)
+    phone_verified = models.BooleanField(default=True)
     email_verified_at = models.DateTimeField(blank=True, null=True)
     phone_verified_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.user.username
+
+
 
