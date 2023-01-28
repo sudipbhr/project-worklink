b59599b13469d9e3b0ae9d8f3ca1fdcc77e4b13f
Sudip <sudipbhandari67@gmail.com>
2023-01-28 11:51:39 +0545

    changes

--- services/models.py
+++ services/models.py
@@ -50,7 +50,7 @@ class Services(models.Model):
     category = models.ManyToManyField(Category, blank=True)
     skills =models.ManyToManyField(JobSkills, blank=True)
     vacancy = models.IntegerField(help_text="Enter number of vacancies", default='1')
-    posted_by= models.ForeignKey(User, on_delete=models.CASCADE, related_name='services')
+    posted_by= models.ForeignKey(User, on_delete=models.CASCADE, related_name='services', null=True)
     location =models.CharField(max_length=200)
     image = models.ImageField(upload_to='services/')
     created_at = models.DateTimeField(auto_now_add=True)
