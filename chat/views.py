5af4effee46a431294e0819bae7bf2d74b14b499
thapaasha <ashathapa9232@gmail.com>
2023-02-09 23:18:06 +0545

    chats added

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from chat.models import Chat
 
 # Create your views here.
+
 def chats(request):
+    messages = Chat.objects.all()
     template_name= 'chat/chats.html'
-    context={}
-    return render(request, template_name, context)
+    context={
+        'messages' : messages
+    }
+    return render (request, template_name, context)
